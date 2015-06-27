27b5052c0c26a111371bbe58ccc0b7133d28981b
nikolakn <nkcodeplus@gmail.com>
2015-06-28 00:12:49 +0200

    epoch

--- src/nk/code/data/Epoch.java
+++ src/nk/code/data/Epoch.java
@@ -3,6 +3,7 @@ package nk.code.data;
 import nk.code.doc.NkSkala;
 import nk.code.epoch.ScalaView;
 import android.graphics.Canvas;
+import android.graphics.Paint.Align;
 
 public class Epoch extends Event {
 
@@ -24,25 +25,42 @@ public class Epoch extends Event {
     	        canvas.drawLine(x, 0, x, skala.getHeight(), mPaint);
     	        canvas.save();
     	        canvas.rotate(-90);
-    	        mPaint.setColor(crna);
-    	      
+    	        mPaint.setColor(crna);  	      
     	        canvas.drawText(name,-skala.getHeight()/2, x-4 , mPaint);
     	        canvas.restore();
         	}
         	if(y2 >= 0 && y2 != NkSkala.BELOW) {
         	//donji kraj se savrsava u prozoru	
         		canvas.drawLine(x, 0, x, y2, mPaint);
+    	        canvas.save();
+    	        canvas.rotate(-90);
+    	        mPaint.setColor(crna);  	      
+    	        canvas.drawText(name,-y2, x-4 , mPaint);
+    	        canvas.restore();
         	}
         }        		
         if(y != NkSkala.BELOW && y != NkSkala.ABOVE ) {
         	if(y2==NkSkala.BELOW){
         	//gornji kraj pocenje u prozoru
         		canvas.drawLine(x, y, x, skala.getHeight(), mPaint);
-        		
+    	        canvas.save();
+    	        canvas.rotate(-90);
+    	        mPaint.setColor(crna);  
+    	        mPaint.setTextAlign(Align.RIGHT);
+    	        canvas.drawText(name,-y, x-4 , mPaint);
+    	        mPaint.setTextAlign(Align.LEFT);
+    	        canvas.restore();       		
         	}
         	if(y2 >= 0 && y2 != NkSkala.BELOW) {
         	//cela je u prozoru	
         		canvas.drawLine(x, y, x, y2, mPaint);
+    	        canvas.save();
+    	        canvas.rotate(-90);
+    	        mPaint.setColor(crna);  
+    	        mPaint.setTextAlign(Align.CENTER);
+    	        canvas.drawText(name,-(y+(y2-y)/2), x-4 , mPaint);
+    	        mPaint.setTextAlign(Align.LEFT);
+    	        canvas.restore();
         	}	
         }		
 	}
