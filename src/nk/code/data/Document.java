67bbb196e864f3db3acb4662fa83e428b80f3aa0
nikolakn <nkcodeplus@gmail.com>
2015-06-27 20:17:20 +0200

    doc

--- src/nk/code/data/Document.java
+++ src/nk/code/data/Document.java
@@ -2,6 +2,7 @@ package nk.code.data;
 
 import java.util.ArrayList;
 
+import nk.code.doc.NkSkala;
 import nk.code.epoch.ScalaView;
 
 import org.joda.time.DateTime;
@@ -17,13 +18,14 @@ public class Document {
 	private ArrayList<Event> list = new ArrayList<Event>();
 	private Paint mPaint;
 	
+	public static int zelena = 0xFF00FF00;
+	public static int crna = 0xFF000000;
+	
 	public Document(){
 		mPaint = new Paint();
 		mPaint.setAntiAlias(true);
 		mPaint.setTextSize(16);
-		//mPaint.setColor(0xFFF00000);
-
-		mPaint.setColor(0xFF00FF00);
+		mPaint.setColor(zelena);
 	}
 	
 	public void addEvent(Event e){
@@ -41,15 +43,45 @@ public class Document {
 	
 	public void draw(Canvas canvas, ScalaView skala){
         for(Event e : list){
-	        float y = skala.getPos(e.start);
-	        if(y>=0){
-	        	mPaint.setColor(0xFF00FF00);
-		        canvas.drawRect(e.x, y-e.size/2,e.x+e.size, y+e.size/2, mPaint);
-		        mPaint.setColor(0xFF000000);
-		        Rect bounds = new Rect();
-		        mPaint.getTextBounds(e.name, 0, e.name.length(), bounds);
-		        canvas.drawText(e.name, e.x+e.size+5, y+ bounds.height()/2, mPaint);
-	        }
+        //Event
+        
+        	if(e.getClass().isInstance(Event.class)){
+		        float y = skala.getPos(e.start);
+		        if(y>=0){
+		        	mPaint.setColor(zelena);
+			        canvas.drawRect(e.x, y-e.size/2,e.x+e.size, y+e.size/2, mPaint);
+			        mPaint.setColor(crna);
+			        Rect bounds = new Rect();
+			        mPaint.getTextBounds(e.name, 0, e.name.length(), bounds);
+			        canvas.drawText(e.name, e.x+e.size+5, y+ bounds.height()/2, mPaint);
+		        }
+        	}
+        	//Epoch
+        	if(e.getClass().isInstance(Epoch.class)){
+        		Epoch ep = (Epoch)e;
+		        float y = skala.getPos(ep.start);
+		        float y2 = skala.getPos(ep.end);
+		        if(y==NkSkala.ABOVE){
+		        	if(y2==NkSkala.BELOW){
+		        	//prolazi kroz ceo prozor
+		        		
+		        	}
+		        	if(y2 >= 0 && y2 != NkSkala.BELOW) {
+		        	//donji kraj se savrsava u prozoru	
+		        	}
+		        }        		
+		        if(y != NkSkala.BELOW && y != NkSkala.ABOVE ) {
+		        	if(y2==NkSkala.BELOW){
+		        	//gornji kraj pocenje u prozoru
+		        		
+		        	}
+		        	if(y2 >= 0 && y2 != NkSkala.BELOW) {
+		        	//cela je u prozoru	
+		        	}	
+		        }
+        		
+        	}
+        	
         }
 	
 	}
