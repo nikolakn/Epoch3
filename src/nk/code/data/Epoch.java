541aeb31ea896dde90175f46d3f4c61bb8042517
nikolakn <nkcodeplus@gmail.com>
2015-06-28 00:19:22 +0200

    boja

--- src/nk/code/data/Epoch.java
+++ src/nk/code/data/Epoch.java
@@ -7,61 +7,61 @@ import android.graphics.Paint.Align;
 
 public class Epoch extends Event {
 
-	public Epoch(double s,int x, String n) {
+	public Epoch(double s, int x, String n) {
 		super(s, x, n);
 		// TODO Auto-generated constructor stub
 	}
 
 	public double end;
-	
+
 	@Override
 	public void draw(Canvas canvas, ScalaView skala) {
-        float y = skala.getPos(start);
-        float y2 = skala.getPos(end);
-        mPaint.setColor(zelena);
-        if(y==NkSkala.ABOVE){
-        	if(y2==NkSkala.BELOW){
-        	//prolazi kroz ceo prozor
-    	        canvas.drawLine(x, 0, x, skala.getHeight(), mPaint);
-    	        canvas.save();
-    	        canvas.rotate(-90);
-    	        mPaint.setColor(crna);  	      
-    	        canvas.drawText(name,-skala.getHeight()/2, x-4 , mPaint);
-    	        canvas.restore();
-        	}
-        	if(y2 >= 0 && y2 != NkSkala.BELOW) {
-        	//donji kraj se savrsava u prozoru	
-        		canvas.drawLine(x, 0, x, y2, mPaint);
-    	        canvas.save();
-    	        canvas.rotate(-90);
-    	        mPaint.setColor(crna);  	      
-    	        canvas.drawText(name,-y2, x-4 , mPaint);
-    	        canvas.restore();
-        	}
-        }        		
-        if(y != NkSkala.BELOW && y != NkSkala.ABOVE ) {
-        	if(y2==NkSkala.BELOW){
-        	//gornji kraj pocenje u prozoru
-        		canvas.drawLine(x, y, x, skala.getHeight(), mPaint);
-    	        canvas.save();
-    	        canvas.rotate(-90);
-    	        mPaint.setColor(crna);  
-    	        mPaint.setTextAlign(Align.RIGHT);
-    	        canvas.drawText(name,-y, x-4 , mPaint);
-    	        mPaint.setTextAlign(Align.LEFT);
-    	        canvas.restore();       		
-        	}
-        	if(y2 >= 0 && y2 != NkSkala.BELOW) {
-        	//cela je u prozoru	
-        		canvas.drawLine(x, y, x, y2, mPaint);
-    	        canvas.save();
-    	        canvas.rotate(-90);
-    	        mPaint.setColor(crna);  
-    	        mPaint.setTextAlign(Align.CENTER);
-    	        canvas.drawText(name,-(y+(y2-y)/2), x-4 , mPaint);
-    	        mPaint.setTextAlign(Align.LEFT);
-    	        canvas.restore();
-        	}	
-        }		
+		float y = skala.getPos(start);
+		float y2 = skala.getPos(end);
+		mPaint.setColor(colorLine);
+		if (y == NkSkala.ABOVE) {
+			if (y2 == NkSkala.BELOW) {
+				// prolazi kroz ceo prozor
+				canvas.drawLine(x, 0, x, skala.getHeight(), mPaint);
+				canvas.save();
+				canvas.rotate(-90);
+				mPaint.setColor(colorText);
+				canvas.drawText(name, -skala.getHeight() / 2, x - 4, mPaint);
+				canvas.restore();
+			}
+			if (y2 >= 0 && y2 != NkSkala.BELOW) {
+				// donji kraj se savrsava u prozoru
+				canvas.drawLine(x, 0, x, y2, mPaint);
+				canvas.save();
+				canvas.rotate(-90);
+				mPaint.setColor(colorText);
+				canvas.drawText(name, -y2, x - 4, mPaint);
+				canvas.restore();
+			}
+		}
+		if (y != NkSkala.BELOW && y != NkSkala.ABOVE) {
+			if (y2 == NkSkala.BELOW) {
+				// gornji kraj pocenje u prozoru
+				canvas.drawLine(x, y, x, skala.getHeight(), mPaint);
+				canvas.save();
+				canvas.rotate(-90);
+				mPaint.setColor(colorText);
+				mPaint.setTextAlign(Align.RIGHT);
+				canvas.drawText(name, -y, x - 4, mPaint);
+				mPaint.setTextAlign(Align.LEFT);
+				canvas.restore();
+			}
+			if (y2 >= 0 && y2 != NkSkala.BELOW) {
+				// cela je u prozoru
+				canvas.drawLine(x, y, x, y2, mPaint);
+				canvas.save();
+				canvas.rotate(-90);
+				mPaint.setColor(colorText);
+				mPaint.setTextAlign(Align.CENTER);
+				canvas.drawText(name, -(y + (y2 - y) / 2), x - 4, mPaint);
+				mPaint.setTextAlign(Align.LEFT);
+				canvas.restore();
+			}
+		}
 	}
 }
--- src/nk/code/data/Event.java
+++ src/nk/code/data/Event.java
@@ -1,43 +1,42 @@
 package nk.code.data;
 
 import nk.code.epoch.ScalaView;
+import nk.code.helper.Boja;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 
-public class Event {	
+public class Event {
 	public double start = 0;
 	public String name = "";
 	public int size = 20;
 	public boolean hasimage = false;
-	public int look = 1;	
+	public int look = 1;
 	public int x = 50;
-	
+	public int colorLine = Boja.zelena;
+	public int colorText = Boja.crna;
 	protected Paint mPaint;
-	
-	public static int zelena = 0xFF00FF00;
-	public static int crna = 0xFF000000;
-	
-	public Event(double s, int x,String n){
+
+	public Event(double s, int x, String n) {
 		name = n;
 		start = s;
 		this.x = x;
 		mPaint = new Paint();
 		mPaint.setAntiAlias(true);
 		mPaint.setTextSize(16);
-		mPaint.setColor(zelena);
+		mPaint.setColor(colorLine);
 	}
 
 	public void draw(Canvas canvas, ScalaView skala) {
 		// TODO Auto-generated method stub
-        float y = skala.getPos(start);
-        if(y>=0){
-        	mPaint.setColor(zelena);
-	        canvas.drawRect(x, y-size/2,x+size, y+size/2, mPaint);
-	        mPaint.setColor(crna);
-	        Rect bounds = new Rect();
-	        mPaint.getTextBounds(name, 0, name.length(), bounds);
-	        canvas.drawText(name, x+size+5, y+ bounds.height()/2, mPaint);
-        }		
+		float y = skala.getPos(start);
+		if (y >= 0) {
+			mPaint.setColor(colorLine);
+			canvas.drawRect(x, y - size / 2, x + size, y + size / 2, mPaint);
+			mPaint.setColor(colorText);
+			Rect bounds = new Rect();
+			mPaint.getTextBounds(name, 0, name.length(), bounds);
+			canvas.drawText(name, x + size + 5, y + bounds.height() / 2, mPaint);
+		}
 	}
 }
