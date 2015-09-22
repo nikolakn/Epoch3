c136db4d55e66678190e40ad52e2560ace9f5aa5
nikolakn <nkcodeplus@gmail.com>
2015-09-22 21:18:08 +0200

    delete timeline

--- src/nk/code/epoch/MainActivity.java
+++ src/nk/code/epoch/MainActivity.java
@@ -6,6 +6,7 @@ import nk.code.data.EpochDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -17,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -29,11 +31,13 @@ public class MainActivity extends AppCompatActivity {
 	ArrayAdapter<String> adapter;
 	public final static String EXTRA_MESSAGE = "nk.code.epoch.START";
 	public final static String EXTRA_MESSAGE_NEW = "nk.code.epoch.NEW";
+	
+	private Context con; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);   
-		
+		con = this;
 		list = (ListView) findViewById(R.id.mainlistview);
 
 		ArrayList<String> listad = new ArrayList<String>(); 
@@ -73,8 +77,71 @@ public class MainActivity extends AppCompatActivity {
 
          });
 		
-	}
+		list.setOnItemLongClickListener(new OnItemLongClickListener() {
 
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,int pos, long id) {
+                // TODO Auto-generated method stub
+
+                final String  naziv    = (String) list.getItemAtPosition(pos);
+             
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                        case DialogInterface.BUTTON_POSITIVE:
+                            EpochDatabase helper=new EpochDatabase(con);
+                    		SQLiteDatabase database=helper.getWritableDatabase();
+                    		String naslov = EpochDatabase.N_IME +"='"+naziv+"'";
+                    		database.delete(EpochDatabase.N_TABLE, naslov,null);
+                    		
+                    		String selection = EpochDatabase.U_EPOCH +"='"+naziv+"'";
+                    		database.delete(EpochDatabase.U_TABLE, selection,null);
+                    		
+                    		String sk = EpochDatabase.S_EPOCH +"='"+naziv+"'";
+                    		database.delete(EpochDatabase.S_TABLE, sk,null);
+                    		osvezi();
+                            break;
+                           
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            //No button clicked
+                            break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(con);
+                builder.setMessage("Are you sure you want to delete this timeline?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();             
+                
+                return true;
+            }
+        }); 
+		
+	}
+	@Override
+	public void onResume(){
+		super.onResume();  // Always call the superclass method first
+		osvezi();
+	}
+	
+	private void osvezi(){
+		
+		if(adapter != null){
+			adapter.clear();
+			EpochDatabase helper = new EpochDatabase(this);
+			SQLiteDatabase database = helper.getWritableDatabase();
+			Cursor cursor = database.rawQuery("select * from "
+					+ EpochDatabase.N_TABLE, null);
+			cursor.moveToFirst();
+			
+			while (!cursor.isAfterLast()) {
+				String s=cursor.getString(cursor.getColumnIndex(EpochDatabase.N_IME));
+				if(s!=null)
+					adapter.add(s);
+				cursor.moveToNext();
+			}		
+		}
+	}
 	private void startEpoch(String naziv) {
 		Intent intent = new Intent(this, EpochActivity.class);
 		String message = naziv;
